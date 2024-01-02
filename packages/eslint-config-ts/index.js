6f3fb16cc9d9deaa358b24b19362272265ea19a4
Finley Ge <m13203533462@163.com>
2024-01-02 12:12:02 +0800

    feat(ts): support ts

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -2,10 +2,17 @@ module.exports = {
   extends: [
     "@finley_ge/eslint-config-base",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
   ],
   parser: "@typescript-eslint/parser",
   rules: {
     "@typescript-eslint/ban-ts-comment": ["error", { "ts-ignore": "allow-with-description" }],
     "@typescript-eslint/no-explicit-any": "off"
-  }
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
 };
